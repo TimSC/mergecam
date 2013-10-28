141c155412ba55aa0016a006b85e530ad9bdd386
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2013-10-28 07:52:14 +0000

    Video stacking now handled by widget

--- glcam.py
+++ glcam.py
@@ -2,18 +2,18 @@
 Copyright (c) 2013, Tim Sheerman-Chase
 All rights reserved.
 '''
-import sys, time, os
+import sys, time, os, uuid
 from PyQt4 import QtGui, QtCore
 import v4l2capture
 
 class SourceWidget(QtGui.QFrame):
-	def __init__(self, srcId, devManager):
+	def __init__(self, devId, devManager):
 		QtGui.QFrame.__init__(self)
 
 		self.widgetLayout = QtGui.QVBoxLayout()
 		self.setLayout(self.widgetLayout)
 
-		self.srcId = srcId
+		self.devId = devId
 		self.cameraOn = False
 		self.devManager = devManager
 
@@ -21,7 +21,7 @@ class SourceWidget(QtGui.QFrame):
 		self.toolbar = QtGui.QHBoxLayout()
 		self.widgetLayout.addLayout(self.toolbar)
 
-		label = QtGui.QLabel(srcId)
+		label = QtGui.QLabel(devId)
 		self.toolbar.addWidget(label, 0)
 
 		self.onButton = QtGui.QPushButton("On")
@@ -48,11 +48,11 @@ class SourceWidget(QtGui.QFrame):
 	def Update(self):
 
 		if self.cameraOn:
-			data = self.devManager.get_frame(self.srcId)
+			data = self.devManager.get_frame(self.devId)
 
 			if data is not None:
 				#print len(data[0])
-				self.emit(QtCore.SIGNAL('webcam_frame'), data[0], data[1], self.srcId)
+				self.emit(QtCore.SIGNAL('webcam_frame'), data[0], data[1], self.devId)
 				self.UpdatePreview(data[0], data[1])
 
 	def UpdatePreview(self, frame, meta):
@@ -73,15 +73,14 @@ class SourceWidget(QtGui.QFrame):
 
 		if self.cameraOn:
 			self.cameraOn = False
-			self.devManager.stop(self.srcId)
-			self.devManager.close(self.srcId)
+			self.devManager.stop(self.devId)
+			self.devManager.close(self.devId)
 			self.ClearPreview()
-			self.onButton.setChecked(0)
 		else:
 			self.cameraOn = True
-			self.devManager.open(self.srcId)
-			self.devManager.set_format(self.srcId, 640, 480, "MJPEG")
-			self.devManager.start(self.srcId)
+			self.devManager.open(self.devId)
+			self.devManager.set_format(self.devId, 640, 480, "MJPEG")
+			self.devManager.start(self.devId)
 		
 		self.onButton.setChecked(self.cameraOn)
 
@@ -89,7 +88,7 @@ class SourceWidget(QtGui.QFrame):
 		if not self.cameraOn:
 			self.ClickedOn()
 
-		self.emit(QtCore.SIGNAL('use_source_clicked'), self.srcId)
+		self.emit(QtCore.SIGNAL('use_source_clicked'), self.devId)
 
 class VideoOutWidget(QtGui.QFrame):
 	def __init__(self, devId, videoOutManager):
@@ -141,22 +140,112 @@ class VideoOutWidget(QtGui.QFrame):
 
 		self.onButton.setChecked(self.devOn)
 
-	def SendFrame(self, pixmap, meta):
+	def SendFrame(self, frame, meta, devName):
 		if not self.devOn: return
+		if meta['format'] != "RGB24": return
 
-		pixmap2 = pixmap.scaled(640, 480)
+		im2 = QtGui.QImage(frame, meta['width'], meta['height'], QtGui.QImage.Format_RGB888)
+		pix = QtGui.QPixmap(im2)
+		pixmap2 = pix.scaled(640, 480)
 		img = pixmap2.toImage()
 		img2 = img.convertToFormat(QtGui.QImage.Format_RGB888)
 		raw = img2.bits().asstring(img2.numBytes())
 		self.videoOutManager.send_frame(self.devId, str(raw), "RGB24", 
 			img2.width(), img2.height())
 
+	def Update(self):
+
+		if self.cameraOn:
+			if 0:
+				#print len(data[0])
+				self.emit(QtCore.SIGNAL('webcam_frame'), data[0], data[1], self.devId)
+				self.UpdatePreview(data[0], data[1])
+
+
+class GridStackWidget(QtGui.QFrame):
+	def __init__(self, devInputs):
+		QtGui.QFrame.__init__(self)
+		self.devOn = False
+		self.devId = uuid.uuid4()
+		self.devInputs = devInputs
+		self.canvas = QtGui.QImage(640*2, 480*2, QtGui.QImage.Format_RGB888)
+		self.framesRcvSinceOutput = set()
+		self.outBuffer = []
+
+		self.widgetLayout = QtGui.QVBoxLayout()
+		self.setLayout(self.widgetLayout)
+
+		#Create toolbar
+		self.toolbar = QtGui.QHBoxLayout()
+		self.widgetLayout.addLayout(self.toolbar)
+
+		label = QtGui.QLabel("Stack Videos")
+		self.toolbar.addWidget(label, 0)
+
+		self.onButton = QtGui.QPushButton("On")
+		self.toolbar.addWidget(self.onButton, 0)
+		self.onButton.setCheckable(True)
+		QtCore.QObject.connect(self.onButton, QtCore.SIGNAL('clicked()'), self.ClickedOn)
+
+		self.useButton = QtGui.QPushButton("Use")
+		self.toolbar.addWidget(self.useButton, 0)
+		QtCore.QObject.connect(self.useButton, QtCore.SIGNAL('clicked()'), self.ClickedUse)
+
+		self.setFrameStyle(QtGui.QFrame.Box)
+		self.setSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.MinimumExpanding)
+
+	def ClickedOn(self):
+
+		if self.devOn:
+			self.devOn = False
+		else:
+			self.devOn = True
+		print self.devOn
+		self.onButton.setChecked(self.devOn)
+
+	def SendFrame(self, frame, meta, devName):
+		if not self.devOn: return
+		if devName not in self.devInputs: return
+
+		devIndex = self.devInputs.index(devName)
+		x = devIndex / 2
+		y = devIndex % 2
+
+		img = QtGui.QImage(frame, meta['width'], meta['height'], QtGui.QImage.Format_RGB888)
+		
+		painter = QtGui.QPainter(self.canvas)
+		painter.setRenderHint(QtGui.QPainter.Antialiasing)
+		painter.drawImage(640 * x, 480 * y, img)
+		del painter
+
+		if devName in self.framesRcvSinceOutput:
+			#We have received this frame again; it is time to write output
+			raw = self.canvas.bits().asstring(self.canvas.numBytes())
+			metaOut = {'width': self.canvas.width(), 'height': self.canvas.height(), 'format': 'RGB24'}
+			self.outBuffer.append([raw, metaOut])
+			self.framesRcvSinceOutput = set()
+
+		self.framesRcvSinceOutput.add(devName)
+
+	def Update(self):
+		for result in self.outBuffer:
+			self.emit(QtCore.SIGNAL('webcam_frame'), result[0], result[1], self.devId)
+		self.outBuffer = []
+
+	def ClickedUse(self):
+		if not self.devOn:
+			self.ClickedOn()
+
+		self.emit(QtCore.SIGNAL('use_source_clicked'), self.devId)
+
+		
 class MainWindow(QtGui.QMainWindow):
 	def __init__(self):
 		super(MainWindow, self).__init__() 
 		self.currentFrames = {}
 		self.inputDeviceToWidgetDict = {}
 		self.outputDeviceToWidgetDict = {}
+		self.processingWidgets = {}
 		self.currentSrcId = None
 
 		self.vidOut = v4l2capture.Video_out_manager()
@@ -239,6 +328,12 @@ class MainWindow(QtGui.QMainWindow):
 			self.sourceList.addWidget(widget)
 			self.inputDeviceToWidgetDict[fina] = widget
 
+		widget = GridStackWidget(self.devNames)
+		QtCore.QObject.connect(widget, QtCore.SIGNAL("webcam_frame"), self.ProcessFrame)
+		QtCore.QObject.connect(widget, QtCore.SIGNAL("use_source_clicked"), self.ChangeVideoSource)
+		self.sourceList.addWidget(widget)
+		self.processingWidgets[widget.devId] = widget
+
 		for fina in self.vidOut.list_devices():
 			
 			widget = VideoOutWidget(fina, self.vidOut)
@@ -247,7 +342,13 @@ class MainWindow(QtGui.QMainWindow):
 
 	def ProcessFrame(self, frame, meta, devName):
 
-		if devName == self.currentSrcId:
+		#Send frames to processing widgets
+		for devId in self.processingWidgets:
+			procWidget = self.processingWidgets[devId]
+			procWidget.SendFrame(frame, meta, devName)
+
+		#Update GUI with new frame
+		if devName == self.currentSrcId and meta['format'] == "RGB24":
 			self.scene.clear()
 			im2 = QtGui.QImage(frame, meta['width'], meta['height'], QtGui.QImage.Format_RGB888)
 			pix = QtGui.QPixmap(im2)
@@ -255,10 +356,11 @@ class MainWindow(QtGui.QMainWindow):
 			gpm = QtGui.QGraphicsPixmapItem(pix)
 			self.scene.addItem(gpm)
 
-			#Send frame to output device
+		#Send frame to output device
+		if devName == self.currentSrcId:
 			for outDevName in self.outputDeviceToWidgetDict:
 				outWidget = self.outputDeviceToWidgetDict[outDevName]
-				outWidget.SendFrame(pix, meta)
+				outWidget.SendFrame(frame, meta, devName)
 
 	def OldFunc(self):
 		try:
@@ -304,6 +406,10 @@ class MainWindow(QtGui.QMainWindow):
 		for fina in self.inputDeviceToWidgetDict:
 			camWidget = self.inputDeviceToWidgetDict[fina]
 			camWidget.Update()
+
+		for fina in self.processingWidgets:
+			procWidget = self.processingWidgets[fina]
+			procWidget.Update()
 
 	def ChangeVideoSource(self, srcId):
 		print "ChangeVideoSource", srcId
