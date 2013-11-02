d7cb1cfcbce9255a877deb775854dc86d4f5d644
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2013-11-02 07:17:03 +0000

    Convert image to iplimage

--- vidpano.py
+++ vidpano.py
@@ -1,6 +1,58 @@
 
 from PyQt4 import QtGui, QtCore
 import uuid
+import cv2, cv
+import numpy as np
+
+### Utility functions
+
+def DetectAcrossImage(img, detector, targetPatchSize = 100.):
+
+	wsteps = int(round(img.shape[1] / targetPatchSize))
+	hsteps = int(round(img.shape[0] / targetPatchSize))
+	if wsteps == 0: wsteps = 1
+	if hsteps == 0: hsteps = 1
+
+	wvals = np.linspace(0, img.shape[1], wsteps)
+	hvals = np.linspace(0, img.shape[0], wsteps)
+	margin = 30
+	out = []
+	for w in range(len(wvals)-1):
+		for h in range(len(hvals)-1):	
+			rowInd = np.arange(int(hvals[h]-margin),int(hvals[h+1]+margin),1)
+			rowInd = np.mod(rowInd, img.shape[0])
+			colInd = np.arange(int(wvals[w]-margin),int(wvals[w+1]+margin),1)
+			colInd = np.mod(colInd, img.shape[1])
+			patch = img[rowInd, :]
+			patch = patch[:, colInd]
+			assert patch.shape[0] > 0
+			assert patch.shape[1] > 0
+
+			#print wvals[w], hvals[h], patch.shape
+			kps = detector.detect(patch)
+			for kp in kps:
+				kp.pt = (kp.pt[0]+wvals[w]-margin, kp.pt[1]+hvals[h]-margin)
+				out.append(kp)
+	return out
+
+
+def GetKeypointsAndDescriptors(im1):
+	detector = cv2.FeatureDetector_create("ORB")
+	#print detector.getParams()
+	detector.setInt("nFeatures", 50)
+	descriptor = cv2.DescriptorExtractor_create("BRIEF")
+
+	grey1 = cv2.cvtColor(im1,cv2.COLOR_BGR2GRAY)
+
+	#print "Extracting points of interest 1"
+	#keypoints1 = detector.detect(grey1)
+	keypoints1 = DetectAcrossImage(grey1, detector)
+	#VisualiseKeypoints(grey1, keypoints1)
+	(keypoints1, descriptors1) = descriptor.compute(grey1, keypoints1)
+	return (keypoints1, descriptors1)
+
+
+### Controlling widget
 
 class PanoWidget(QtGui.QFrame):
 	def __init__(self, devInputs):
@@ -39,6 +91,10 @@ class PanoWidget(QtGui.QFrame):
 
 		self.calibrationCount = QtGui.QLabel("0")
 		self.calibrateControls.addWidget(self.calibrationCount, 1)
+
+		self.onButton = QtGui.QPushButton("Cal")
+		self.calibrateControls.addWidget(self.onButton, 0)
+		QtCore.QObject.connect(self.onButton, QtCore.SIGNAL('clicked()'), self.ClickedCalibrate)
 
 		#Create view controls
 
@@ -92,6 +148,16 @@ class PanoWidget(QtGui.QFrame):
 		#Update GUI
 		self.calibrationCount.setText(str(len(self.calibrationFrames)))
 
+	def ClickedCalibrate(self):
+		for photoSet, metaSet in zip(self.calibrationFrames, self.calibrationMeta):
+			for frame, meta in zip(photoSet, metaSet):
+				assert meta['format'] == "RGB24"
+				arr = np.array(frame, dtype=np.uint8)
+				source = arr.reshape((meta['height'], meta['width'], 3))
+
+				bitmap = cv.CreateImageHeader((source.shape[1], source.shape[0]), cv.IPL_DEPTH_8U, 3)
+				cv.SetData(bitmap, source.tostring(), source.dtype.itemsize * 3 * source.shape[1])
+				cv.SaveImage("test.png", bitmap)
 
 	def SendFrame(self, frame, meta, devName):
 
