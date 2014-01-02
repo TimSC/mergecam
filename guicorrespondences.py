b4135fbcad1dc635109e7013badb4d4fe0c90a92
TimSC <t.sheerman-chase@surrey.ac.uk>
2014-01-02 18:06:31 +0000

    Basic layout in correspondence gui

--- guicorrespondences.py
+++ guicorrespondences.py
@@ -2,10 +2,26 @@ from PySide import QtGui, QtCore
 
 class GuiCorrespondences(QtGui.QFrame):
 
-	def __init__(self, devManager):
+	def __init__(self, correspondenceModel):
 		QtGui.QFrame.__init__(self)
 
-		self.layout = QtGui.QHBoxLayout()
-		self.setLayout(self.layout)
+		self.correspondenceModel = correspondenceModel
 
-		self.layout.addWidget(QtGui.QLabel("test"))
+		self.layout = QtGui.QHBoxLayout()
+		self.mainSplitter = QtGui.QSplitter(QtCore.Qt.Vertical)
+		self.setLayout(self.layout)
+		self.layout.addWidget(self.mainSplitter)
+
+		self.splitLayout = QtGui.QHBoxLayout()
+		self.splitWidget = QtGui.QFrame()
+		self.splitWidget.setLayout(self.splitLayout)
+		self.mainSplitter.addWidget(self.splitWidget)
+
+		self.tableFrame = QtGui.QFrame()
+		self.tableLayout = QtGui.QHBoxLayout()
+		self.tableFrame.setLayout(self.tableLayout)
+		self.mainSplitter.addWidget(self.tableFrame)
+
+		self.splitLayout.addWidget(QtGui.QLabel("Left"))
+		self.splitLayout.addWidget(QtGui.QLabel("Right"))
+		self.tableLayout.addWidget(QtGui.QLabel("Table"))
